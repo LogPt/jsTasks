5024194f9509b96ba769e58c38fae67f12e4a606
LogPT <logunovpa@gmail.com>
2023-03-24 02:01:26 +0600

    Доделал упражнения с запросами Fetch и XHR

--- ex-request/ex-request.js
+++ ex-request/ex-request.js
@@ -1,6 +1,33 @@
+const message = document.querySelector('.message');
 const requestUrl = 'https://jsonplaceholder.typicode.com/users';
 
 function sendRequest(method, url, body = null) {
+  const param = {
+    method: method,
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  if (method !== 'POST') {
+    param.body = null;
+  }
+  // console.log(param);
+
+  return fetch(url, param).then(responce => {
+    if (responce.ok) {
+      return responce.json()
+    }
+    return responce.json().then(error => {
+      const er = new Error(`Ошибка ${error}`);
+      er.data = error;
+      throw er;
+    })
+  })
+}
+
+function sendXhrRequest(method, url, body = null) {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
@@ -22,14 +49,121 @@ function sendRequest(method, url, body = null) {
   })
 }
 
-const body = {
+const bodyToSend = {
   name: 'Jack',
-  age: 20
+  age: 20,
+  city: 'NY',
+  gender: 'male'
+};
+
+
+function clearClick() {
+  message.style.display = 'none';
+  message.innerHTML = null;
 }
 
-// sendRequest('GET', requestUrl)
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
-sendRequest('POST', requestUrl, body)
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+function fetchPostClick() {
+  if (message.style.display !== 'none') {
+    clearClick();
+  }
+  message.style.display = 'block';
+  let messageHeader = `<h3>Запрос Fetch по адресу <br> <a href="${requestUrl}">${requestUrl}</a></h3>`;
+  message.innerHTML = messageHeader;
+  let messageSubHeader = document.createElement('h4');
+  let messageString = document.createElement('p');
+
+  messageSubHeader.innerText = 'Запрос типа POST';
+  message.append(messageSubHeader);
+  sendRequest('POST', requestUrl, bodyToSend)
+    .then(data => {
+      console.log(data);
+      messageString.innerHTML = `<p>${JSON.stringify(data)}</p>`;
+      message.append(messageString);
+    })
+    .catch(err => {
+      console.log(err);
+      messageString.innerText = JSON.stringify(err);
+      message.append(messageString);
+    });
+}
+
+function fetchGetClick() {
+  if (message.style.display !== 'none') {
+    clearClick();
+  }
+  message.style.display = 'block';
+  let messageHeader = `<h3>Запрос Fetch по адресу <br> <a href="${requestUrl}">${requestUrl}</a></h3>`;
+  message.innerHTML = messageHeader;
+  let messageSubHeader = document.createElement('h4');
+  let messageString = document.createElement('p');
+
+  messageSubHeader.innerText = 'Запрос типа GET';
+  message.append(messageSubHeader);
+  sendRequest('GET', requestUrl)
+    .then(data => {
+      console.log(data);
+      data.forEach(el => {
+        console.log(el);
+        messageString.innerHTML += `<p>${JSON.stringify(el)}</p>`;
+      });
+      message.append(messageString);
+    })
+    .catch(err => {
+      console.log(err);
+      messageString.innerText = JSON.stringify(err);
+      message.append(messageString);
+    });
+}
+
+function xhrPostClick() {
+  if (message.style.display !== 'none') {
+    clearClick();
+  }
+  message.style.display = 'block';
+  let messageHeader = `<h3>Запрос XHR по адресу <br> <a href="${requestUrl}">${requestUrl}</a></h3>`;
+  message.innerHTML = messageHeader;
+  let messageSubHeader = document.createElement('h4');
+  let messageString = document.createElement('p');
+
+  messageSubHeader.innerText = 'Запрос типа POST';
+  message.append(messageSubHeader);
+  sendXhrRequest('POST', requestUrl, bodyToSend)
+    .then(data => {
+      console.log(data);
+      messageString.innerHTML = `<p>${JSON.stringify(data)}</p>`;
+      message.append(messageString);
+    })
+    .catch(err => {
+      console.log(err);
+      messageString.innerText = JSON.stringify(err);
+      message.append(messageString);
+    });
+}
+
+function xhrGetClick() {
+  if (message.style.display !== 'none') {
+    clearClick();
+  }
+  message.style.display = 'block';
+  let messageHeader = `<h3>Запрос XHR по адресу <br> <a href="${requestUrl}">${requestUrl}</a></h3>`;
+  message.innerHTML = messageHeader;
+  let messageSubHeader = document.createElement('h4');
+  let messageString = document.createElement('p');
+
+  messageSubHeader.innerText = 'Запрос типа GET';
+  message.append(messageSubHeader);
+  sendXhrRequest('GET', requestUrl)
+    .then(data => {
+      console.log(data);
+      data.forEach(el => {
+        console.log(el);
+        messageString.innerHTML += `<p>${JSON.stringify(el)}</p>`;
+      });
+      message.append(messageString);
+    })
+    .catch(err => {
+      console.log(err);
+      messageString.innerText = JSON.stringify(err);
+      message.append(messageString);
+    });
+}
