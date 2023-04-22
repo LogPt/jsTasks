ac90c7166f035779cae862255a35868c185fa9b1
LogPt <logunovpa@gmail.com>
2023-04-22 19:05:04 +0600

    Event loop изучаю

--- ex-event-loop/ex-event-loop.js
+++ ex-event-loop/ex-event-loop.js
@@ -52,3 +52,50 @@ clear.addEventListener('click', () => {
 
 console.log('code ends');
 fillText('code ends');
+
+//  2 переменные let и var  ведут по разному в этом коде
+for (let i = 0; i < 10; i++) {
+  console.log('outside setTimeout i =>>>', i);
+  setTimeout(function () {
+    console.log('inside setTimeout i =>>>', i);
+  }, 100);
+}
+
+// About Scope
+
+ var name = "Вася";
+
+function sayHi() {
+  console.log('inside sayHi name =>>>', name);
+  console.log('inside sayHi typeof name =>>>',
+    typeof (name));
+}
+
+setTimeout(function () {
+  var name = "Петя";
+    console.log('inside setTimeout name =>>>', name);
+  sayHi();
+}, 1000); 
+
+console.log('inside global scope name =>>>', name);
+
+
+// индикация прогресса
+let i = 0;
+
+function count() {
+  
+  
+  // сделать часть крупной задачи (*)
+  do {
+    i++;
+    progress.innerHTML = i;
+  } while (i % 1e3 != 0);
+  if (i < 1e6) {
+    setTimeout(count);
+    // queueMicrotask(count); //  отрисовка страницы происходит только в самом конце
+  }
+}
+
+
+count();
